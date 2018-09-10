8d9bf38d20e98b9fb118785644c6d3cd94361cb9
Brandon Minnick <brminnick@users.noreply.github.com>
2018-09-10 14:01:24 -0700

    Uncommented Error

--- SentimentAnalysis/Constants/CognitiveServicesConstants.cs
+++ SentimentAnalysis/Constants/CognitiveServicesConstants.cs
@@ -2,10 +2,10 @@
 {
     static class CognitiveServicesConstants
     {
-        //#error Missing API Key. Create an API Key at https://aka.ms/cognitive-services-text-analytics-api
+        #error Missing API Key. Create an API Key at https://aka.ms/cognitive-services-text-analytics-api
         public const string TextSentimentAPIKey = "Your API Key";
 
-        //#error Missing Endpoint for the Cognitive Servce. 
+        #error Missing Endpoint for the Cognitive Servce. 
         //Example Endpoint: https://westus.api.cognitive.microsoft.com
         public const string BaseUrl = "Your Text API Base Url";
     }
