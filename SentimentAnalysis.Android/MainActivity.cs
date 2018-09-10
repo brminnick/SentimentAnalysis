e66fa052f44510614653edd0452b4abc7277f025
Brandon Minnick <brminnick@users.noreply.github.com>
2018-09-10 14:29:19 -0700

    Resolved Android Package References

--- SentimentAnalysis.Android/MainActivity.cs
+++ SentimentAnalysis.Android/MainActivity.cs
@@ -1,26 +1,21 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace SentimentAnalysis.Droid
 {
-	[Activity (Label = "SentimentAnalysis", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity (Label = "SentimentAnalysis", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
-		protected override void OnCreate (Bundle bundle)
+		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			TabLayoutResource = Resource.Layout.Tabbar;
 			ToolbarResource = Resource.Layout.Toolbar; 
 
-			base.OnCreate (bundle);
+			base.OnCreate (savedInstanceState);
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
-			LoadApplication (new SentimentAnalysis.App ());
+			global::Xamarin.Forms.Forms.Init (this, savedInstanceState);
+			LoadApplication (new App());
 		}
 	}
 }
